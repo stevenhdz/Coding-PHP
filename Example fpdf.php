de9e40c696ecc6de972829828f1ab004123a7d8c
Alexander Jimenez <stevenhernandezj@gmail.com>
2020-09-28 00:23:13 -0500

    Initial commit

---
+++ Example fpdf.php
@@ -0,0 +1,114 @@
+<?php
+  require('fpdf182/fpdf.php');
+
+        $saludo = 'Hola mundo';
+        $uno = 28;
+        $dos = true;
+        $tres = 23.2;
+        $cuatro = array("b" => "bar");
+        
+
+       /*  $ventanas = 5;
+        $this->ventana = $nro;  */
+
+        class PDF extends FPDF
+{
+  
+// Cabecera de página
+function Header()
+{
+    // Logo
+    $this->Image('logo.gif',10,8,33);
+    // Arial bold 15
+    $this->SetFont('Arial','B',15);
+    // Movernos a la derecha
+    $this->Cell(80);
+    // Título
+    $this->Cell(30,10,'My page',1,0,'C');
+    // Salto de línea
+    $this->Ln(20);
+}
+
+
+function FancyTable($header, $data)
+{
+    // Colores, ancho de línea y fuente en negrita
+    $this->SetFillColor(255,0,0);
+    $this->SetTextColor(255);
+    $this->SetDrawColor(128,0,0);
+    $this->SetLineWidth(.3);
+    $this->SetFont('','B');
+    // Cabecera
+    $w = array(40, 35, 45, 40);
+    for($i=0;$i<count($header);$i++)
+        $this->Cell($w[$i],7,$header[$i],1,0,'C',true);
+    $this->Ln();
+    // Restauración de colores y fuentes
+    $this->SetFillColor(224,235,255);
+    $this->SetTextColor(0);
+    $this->SetFont('');
+    // Datos
+    $fill = false;
+    foreach($data as $row)
+    {
+        $this->Cell($w[0],6,$row[0],'LR',0,'L',$fill);
+        $this->Cell($w[1],6,$row[1],'LR',0,'L',$fill);
+        $this->Cell($w[2],6,number_format($row[2]),'LR',0,'R',$fill);
+        $this->Cell($w[3],6,number_format($row[3]),'LR',0,'R',$fill);
+        $this->Ln();
+        $fill = !$fill;
+    }
+    // Línea de cierre
+    $this->Cell(array_sum($w),0,'','T');
+}
+
+function BasicTable($header, $data)
+{
+   
+}
+
+function ImprovedTable($header, $data)
+{
+    
+}
+
+// Pie de página
+function Footer()
+{
+    // Posición: a 1,5 cm del final
+    $this->SetY(-15);
+    // Arial italic 8
+    $this->SetFont('Arial','I',8);
+    // Número de página
+    $this->Cell(0,10,'Pagina '.$this->PageNo().'/{nb}',0,0,'C');
+}
+}
+
+// Creación del objeto de la clase heredada
+$pdf = new PDF();
+$pdf->AliasNbPages();
+$pdf->AddPage();
+$pdf->SetFont('Times','',12);
+$pdf->SetFont('Arial','',14);
+$header = array($saludo,$uno,$dos,$tres,$cuatro,$quinto);
+$pdf->BasicTable($header,$data);
+$pdf->FancyTable($header,$data);
+$pdf->ImprovedTable($header,$data);
+$pdf->Output();  
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Example Library fpdf</title>
+</head>
+
+<body>
+
+
+</body>
+
+</html>
