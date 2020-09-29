958c3c84988235c8d2154bde2145aadc8a081bef
Alexander Jimenez <stevenhernandezj@gmail.com>
2020-09-28 23:04:04 -0500

    Api terminada
    Api terminada

--- Api/db.php
+++ Api/db.php
@@ -1,6 +1,6 @@
 <?php
 
-class DB{
+class DB {
     private $host;
     private $db;
     private $user;
@@ -10,8 +10,8 @@ class DB{
     public function __construct(){
         $this->host = 'localhost:8889';
         $this->db = 'peliculas';
-        $this->user = 'root';
-        $this->password = '';
+        $this->user = 'prueba';
+        $this->password = 'hacker2012.L';
         $this->charset = 'utf8mb4';
     }
 
@@ -22,7 +22,6 @@ class DB{
                 PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_EMULATE_PREPARES   => false,
             ];
-            //$pdo = new PDO($connection, $this->user, $this->password, $options);
             $pdo = new PDO($connection,$this->user,$this->password);
             echo '<script language="javascript">alert("Conexion establecida");</script>';
             return $pdo;
--- Api/index.php
+++ Api/index.php
@@ -3,6 +3,19 @@ include_once 'apipeliculas.php';
 
 $api = new ApiPeliculas();
 
-$api->getAll();
+if(isset($_GET['id'])){
+
+    $id = $_GET['id'];
+
+    if(is_numeric($id)){
+        $api->getById($id);
+    }
+    else{
+        $api->error('Los parametros son incorrectos');
+    }
+}
+else{
+    $api->getAll();
+}
 
 ?>
